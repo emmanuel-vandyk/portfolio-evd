799799a5edafc676ccc3e42b74cc74e7ef4cd181
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2023-03-23 05:55:54 -0300

    Dar estilos al footer y reajustar componentes

--- portfolio-evd/src/app/app.module.ts
+++ portfolio-evd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ContactoComponent } from './components/contacto/contacto.component';
     FormacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    ContactoComponent
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
